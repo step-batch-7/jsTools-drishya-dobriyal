13eabb8e3c68a86ab77956fac9ac14a6b593dc30
Drishya Dobriyal <dobriyaldrishya@gmail.com>
2019-12-24 12:42:02 +0530

    refactored findError

--- tailFunction/src/tailLib.js
+++ tailFunction/src/tailLib.js
@@ -4,18 +4,15 @@ const getCountValue = function(value) {
 };
 
 const findError = function(userArgs, isFilePresent) {
-  if (userArgs.includes("-n")) {
-    if (!Number.isInteger(+userArgs[2])) {
-      illegalCount = getCountValue(userArgs[2]);
-      return { errorOccured: `tail: illegal offset -- ${illegalCount}` };
-    }
-    if (!isFilePresent(userArgs[3]))
-      return {
-        errorOccured: `tail: ${userArgs[3]}: No such file or directory`
-      };
-    return {};
+  if (userArgs.includes("-n") && !Number.isInteger(+userArgs[2])) {
+    illegalCount = getCountValue(userArgs[2]);
+    return { errorOccured: `tail: illegal offset -- ${illegalCount}` };
   }
-  if (!isFilePresent(userArgs[1])) {
+  if (userArgs.includes("-n") && !isFilePresent(userArgs[3]))
+    return {
+      errorOccured: `tail: ${userArgs[3]}: No such file or directory`
+    };
+  if (!userArgs.includes("-n") && !isFilePresent(userArgs[1])) {
     return {
       errorOccured: `tail: ${userArgs[1]}: No such file or directory`
     };
