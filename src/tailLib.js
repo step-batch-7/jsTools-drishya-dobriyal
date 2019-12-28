8ff44a4a755930ead9787dcba9b7db034ec169be
Drishya Dobriyal <dobriyaldrishya@gmail.com>
2019-12-28 17:38:22 +0530

    format the code

--- src/tailLib.js
+++ src/tailLib.js
@@ -3,76 +3,93 @@ const isInteger = function(countOption) {
 };
 
 const isPairValid = function(firstTerm, secondTerm) {
-  return firstTerm.includes("-n") && isInteger(secondTerm);
+  return firstTerm.includes('-n') && isInteger(secondTerm);
 };
 
 const getDefaultOption = function(userArgs) {
+  const lastArgsFinder = -1;
   return {
-    filePath: userArgs.slice(-1).join(""),
-    numOfLines: "-10",
-    errorOccurred: ""
+    filePath: userArgs.slice(lastArgsFinder).join(''),
+    numOfLines: '-10',
+    errorOccurred: null
   };
+};
+
+const stringifyErrorOffset = function(illegalOffset) {
+  return `tail: illegal offset -- ${illegalOffset}`;
 };
 
 const parseUserArgs = function(userArgs) {
   const currentOption = getDefaultOption(userArgs);
   const [, optionFirst, optionSecond] = [...userArgs];
 
-  if (!optionFirst.includes("-n")) {
-    currentOption.errorOccurred = null;
+  if (!optionFirst.includes('-n')) {
     return currentOption;
   }
 
-  currentOption.numOfLines = optionFirst.slice(2) || optionSecond;
+  const separateNumber = 2;
+  currentOption.numOfLines = optionFirst.slice(separateNumber) || optionSecond;
   if (isPairValid(optionFirst, currentOption.numOfLines)) {
-    currentOption.errorOccurred = null;
     return currentOption;
   }
 
-  currentOption.errorOccurred = `tail: illegal offset -- ${currentOption.numOfLines}`;
+  currentOption.errorOccurred = stringifyErrorOffset(currentOption.numOfLines);
   return currentOption;
 };
 
 const readContent = function(reader, isFilePresent, filePath) {
-  if (!isFilePresent(filePath))
+  if (!isFilePresent(filePath)) {
     return {
       errorOccurred: `tail: ${filePath}: No such file or directory`
     };
-  return { content: reader(filePath, "utf8") };
+  }
+  return { content: reader(filePath, 'utf8') };
+};
+
+const getStartingLineWithPlus = function(totalLength, givenLength) {
+  if (givenLength === '+1' || givenLength === '+0') {
+    return totalLength;
+  }
+  const toAddUpperMostLine = 1;
+  return totalLength + toAddUpperMostLine - givenLength;
 };
 
 const parseStartingLine = function(totalLength, givenLength) {
   let startingLine = givenLength;
 
-  givenLength.includes("+") && (startingLine = totalLength + 1 - givenLength);
-  givenLength.includes("-") && (startingLine = -givenLength);
-  (givenLength == "+1" || givenLength == "+0") && (startingLine = totalLength);
+  if (givenLength.includes('+')) {
+    startingLine = getStartingLineWithPlus(totalLength, givenLength);
+  }
+  if (givenLength.includes('-')) {
+    startingLine = -givenLength;
+  }
 
   return startingLine;
 };
 
 const sortContent = function(content, numOfLines) {
-  const arrayOfContent = content.split("\n");
+  const arrayOfContent = content.split('\n');
   const startingLine = parseStartingLine(arrayOfContent.length, numOfLines);
-  return arrayOfContent.slice(-startingLine).join("\n");
+  return arrayOfContent.slice(-startingLine).join('\n');
+};
+
+const generateErrorObject = function(errorType) {
+  return { outputStreamName: 'errorStream', content: errorType };
 };
 
 const performTail = function(userArgs, reader, isFilePresent) {
-  const errorStream = "errorStream";
-
   const parsedArgs = parseUserArgs(userArgs);
-  if (parsedArgs.errorOccurred)
-    return { outputStreamName: errorStream, content: parsedArgs.errorOccurred };
+  if (parsedArgs.errorOccurred) {
+    return generateErrorObject(parsedArgs.errorOccurred);
+  }
 
   const fileContent = readContent(reader, isFilePresent, parsedArgs.filePath);
-  if (fileContent.errorOccurred)
-    return {
-      outputStreamName: errorStream,
-      content: fileContent.errorOccurred
-    };
+  if (fileContent.errorOccurred) {
+    return generateErrorObject(fileContent.errorOccurred);
+  }
 
   return {
-    outputStreamName: "outputStream",
+    outputStreamName: 'outputStream',
     content: sortContent(fileContent.content, parsedArgs.numOfLines)
   };
 };
--- test/testTailLib.js
+++ test/testTailLib.js
@@ -1,95 +1,95 @@
-const assert = require("chai").assert;
+const assert = require('chai').assert;
 const {
   parseUserArgs,
   readContent,
   sortContent,
   performTail
-} = require("../src/tailLib.js");
+} = require('../src/tailLib.js');
 
-describe("tail", function() {
-  describe("parseUserArgs", function() {
-    it("should give filePath stated and default value if -n is not specified ", function() {
-      const userArguments = ["tail.js", "filePath"];
+describe('tail', function() {
+  describe('parseUserArgs', function() {
+    it('should give filePath stated and default value if -n is not specified ', function() {
+      const userArguments = ['tail.js', 'filePath'];
       const actualValue = parseUserArgs(userArguments);
       const expectedValue = {
-        filePath: "filePath",
-        numOfLines: "-10",
+        filePath: 'filePath',
+        numOfLines: '-10',
         errorOccurred: null
       };
       assert.deepStrictEqual(actualValue, expectedValue);
     });
     it("should give filePath and num of lines as stated if - doesn't follow number ", function() {
-      const userArguments = ["tail.js", "-n", "3", "filePath"];
+      const userArguments = ['tail.js', '-n', '3', 'filePath'];
       const actualValue = parseUserArgs(userArguments);
       const expectedValue = {
-        filePath: "filePath",
-        numOfLines: "3",
+        filePath: 'filePath',
+        numOfLines: '3',
         errorOccurred: null
       };
       assert.deepStrictEqual(actualValue, expectedValue);
     });
-    it("should give filePath and num of lines as stated and - is there with number", function() {
-      const userArguments = ["tail.js", "-n", "-3", "filePath"];
+    it('should give filePath and num of lines as stated and - is there with number', function() {
+      const userArguments = ['tail.js', '-n', '-3', 'filePath'];
       const actualValue = parseUserArgs(userArguments);
       const expectedValue = {
-        filePath: "filePath",
-        numOfLines: "-3",
+        filePath: 'filePath',
+        numOfLines: '-3',
         errorOccurred: null
       };
       assert.deepStrictEqual(actualValue, expectedValue);
     });
-    it("should give filePath and num of lines as stated and + is there with number", function() {
-      const userArguments = ["tail.js", "-n", "+3", "filePath"];
+    it('should give filePath and num of lines as stated and + is there with number', function() {
+      const userArguments = ['tail.js', '-n', '+3', 'filePath'];
       const actualValue = parseUserArgs(userArguments);
       const expectedValue = {
-        filePath: "filePath",
-        numOfLines: "+3",
+        filePath: 'filePath',
+        numOfLines: '+3',
         errorOccurred: null
       };
       assert.deepStrictEqual(actualValue, expectedValue);
     });
-    it("should give illegal offset if userArguments after -n is not a number", function() {
-      const userArguments = ["tail.js", "-n", "-$", "filePath"];
+    it('should give illegal offset if userArguments after -n is not a number', function() {
+      const userArguments = ['tail.js', '-n', '-$', 'filePath'];
       const actualValue = parseUserArgs(userArguments);
       const expectedValue = {
-        filePath: "filePath",
-        numOfLines: "-$",
-        errorOccurred: "tail: illegal offset -- -$"
+        filePath: 'filePath',
+        numOfLines: '-$',
+        errorOccurred: 'tail: illegal offset -- -$'
       };
       assert.deepStrictEqual(actualValue, expectedValue);
     });
-    it("should give line count as joined with -n option", function() {
-      const userArguments = ["tail.js", "-n2", "filePath"];
+    it('should give line count as joined with -n option', function() {
+      const userArguments = ['tail.js', '-n2', 'filePath'];
       const actualValue = parseUserArgs(userArguments);
       const expectedValue = {
-        filePath: "filePath",
-        numOfLines: "2",
+        filePath: 'filePath',
+        numOfLines: '2',
         errorOccurred: null
       };
       assert.deepStrictEqual(actualValue, expectedValue);
     });
-    it("should give illegal if  count as joined with -n option is not an integer", function() {
-      const userArguments = ["tail.js", "-na", "filePath"];
+    it('should give illegal if  count as joined with -n option is not an integer', function() {
+      const userArguments = ['tail.js', '-na', 'filePath'];
       const actualValue = parseUserArgs(userArguments);
       const expectedValue = {
-        filePath: "filePath",
-        numOfLines: "a",
-        errorOccurred: "tail: illegal offset -- a"
+        filePath: 'filePath',
+        numOfLines: 'a',
+        errorOccurred: 'tail: illegal offset -- a'
       };
       assert.deepStrictEqual(actualValue, expectedValue);
     });
   });
 
-  describe("readContent", function() {
-    it("should give the content of the file if file exist ", function() {
+  describe('readContent', function() {
+    it('should give the content of the file if file exist ', function() {
       const reader = function(filePath) {
-        assert.strictEqual(filePath, "filePath");
+        assert.strictEqual(filePath, 'filePath');
         const fileContent = "file content's in string ";
         return fileContent;
       };
-      const filePath = "filePath";
+      const filePath = 'filePath';
       const isFilePresent = function(filePath) {
-        assert.strictEqual(filePath, "filePath");
+        assert.strictEqual(filePath, 'filePath');
         return true;
       };
       const actualValue = readContent(reader, isFilePresent, filePath);
@@ -101,9 +101,9 @@ describe("tail", function() {
       const reader = function(filePath) {
         return;
       };
-      const filePath = "filePath";
+      const filePath = 'filePath';
       const isFilePresent = function(filePath) {
-        assert.strictEqual(filePath, "filePath");
+        assert.strictEqual(filePath, 'filePath');
         return false;
       };
       const actualValue = readContent(reader, isFilePresent, filePath);
@@ -113,96 +113,96 @@ describe("tail", function() {
     });
   });
 
-  describe("sortContent", function() {
-    it("should give last 10 line of file of more than 10 lines with - and num  ", function() {
+  describe('sortContent', function() {
+    it('should give last 10 line of file of more than 10 lines with - and num  ', function() {
       const content =
-        "1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12\n13\n14\n15\n16\n17\n18\n19\n20";
+        '1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12\n13\n14\n15\n16\n17\n18\n19\n20';
       assert.strictEqual(
-        sortContent(content, "-10"),
-        "11\n12\n13\n14\n15\n16\n17\n18\n19\n20"
+        sortContent(content, '-10'),
+        '11\n12\n13\n14\n15\n16\n17\n18\n19\n20'
       );
     });
-    it("should give the total line of file if the content of file is less than the line num from which the sorting should be started  with - and num  ", function() {
-      const content = "1\n2\n3\n4\n5\n6";
-      assert.strictEqual(sortContent(content, "-10"), "1\n2\n3\n4\n5\n6");
+    it('should give the total line of file if the content of file is less than the line num from which the sorting should be started  with - and num  ', function() {
+      const content = '1\n2\n3\n4\n5\n6';
+      assert.strictEqual(sortContent(content, '-10'), '1\n2\n3\n4\n5\n6');
     });
-    it("should give sorted line if +num is given such that lines start from num ", function() {
+    it('should give sorted line if +num is given such that lines start from num ', function() {
       const content =
-        "1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12\n13\n14\n15\n16\n17\n18\n19\n20";
+        '1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12\n13\n14\n15\n16\n17\n18\n19\n20';
       assert.strictEqual(
-        sortContent(content, "+5"),
-        "5\n6\n7\n8\n9\n10\n11\n12\n13\n14\n15\n16\n17\n18\n19\n20"
+        sortContent(content, '+5'),
+        '5\n6\n7\n8\n9\n10\n11\n12\n13\n14\n15\n16\n17\n18\n19\n20'
       );
     });
-    it("should give sorted line if num is given without prefix such that it works similar with - prefix", function() {
+    it('should give sorted line if num is given without prefix such that it works similar with - prefix', function() {
       const content =
-        "1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12\n13\n14\n15\n16\n17\n18\n19\n20";
-      assert.strictEqual(sortContent(content, "5"), "16\n17\n18\n19\n20");
+        '1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12\n13\n14\n15\n16\n17\n18\n19\n20';
+      assert.strictEqual(sortContent(content, '5'), '16\n17\n18\n19\n20');
     });
-    it("should give whole file for +1 ", function() {
+    it('should give whole file for +1 ', function() {
       const content =
-        "1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12\n13\n14\n15\n16\n17\n18\n19\n20";
+        '1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12\n13\n14\n15\n16\n17\n18\n19\n20';
       assert.strictEqual(
-        sortContent(content, "+1"),
-        "1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12\n13\n14\n15\n16\n17\n18\n19\n20"
+        sortContent(content, '+1'),
+        '1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12\n13\n14\n15\n16\n17\n18\n19\n20'
       );
     });
-    it("should give whole file for 0 ", function() {
+    it('should give whole file for 0 ', function() {
       const content =
-        "1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12\n13\n14\n15\n16\n17\n18\n19\n20";
+        '1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12\n13\n14\n15\n16\n17\n18\n19\n20';
       assert.strictEqual(
-        sortContent(content, "0"),
-        "1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12\n13\n14\n15\n16\n17\n18\n19\n20"
+        sortContent(content, '0'),
+        '1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12\n13\n14\n15\n16\n17\n18\n19\n20'
       );
     });
   });
 
-  describe("performTail", function() {
-    it("should give last 10 lines of a file that exist ", function() {
+  describe('performTail', function() {
+    it('should give last 10 lines of a file that exist ', function() {
       const reader = function(filePath) {
-        assert.strictEqual(filePath, "filePath");
-        return "1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12\n13\n14\n15\n16\n17\n18\n19\n20";
+        assert.strictEqual(filePath, 'filePath');
+        return '1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12\n13\n14\n15\n16\n17\n18\n19\n20';
       };
       const isFilePresent = function(filePath) {
-        assert.strictEqual(filePath, "filePath");
+        assert.strictEqual(filePath, 'filePath');
         return true;
       };
-      const userArguments = ["tail.js", "filePath"];
+      const userArguments = ['tail.js', 'filePath'];
       const actualValue = performTail(userArguments, reader, isFilePresent);
       assert.deepStrictEqual(actualValue, {
-        content: "11\n12\n13\n14\n15\n16\n17\n18\n19\n20",
-        outputStreamName: "outputStream"
+        content: '11\n12\n13\n14\n15\n16\n17\n18\n19\n20',
+        outputStreamName: 'outputStream'
       });
     });
-    it("should give illegal count error if -n does not have num after it", function() {
+    it('should give illegal count error if -n does not have num after it', function() {
       const reader = () => {};
       const isFilePresent = function(filePath) {
-        assert.strictEqual(filePath, "filePath");
+        assert.strictEqual(filePath, 'filePath');
         return false;
       };
-      const userArguments = ["tail.js", "-n", "-$", "filePath"];
+      const userArguments = ['tail.js', '-n', '-$', 'filePath'];
       assert.deepStrictEqual(
         performTail(userArguments, reader, isFilePresent),
         {
-          content: `tail: illegal offset -- -$`,
-          outputStreamName: "errorStream"
+          content: 'tail: illegal offset -- -$',
+          outputStreamName: 'errorStream'
         }
       );
     });
-    it("should give no such directory error if file does not exist ", function() {
-      const userArguments = ["tail.js", "-n", "-4", "nonExistingFilePath"];
+    it('should give no such directory error if file does not exist ', function() {
+      const userArguments = ['tail.js', '-n', '-4', 'nonExistingFilePath'];
       const reader = () => {
-        return;
+        return null;
       };
       const isFilePresent = filePath => {
-        assert.strictEqual(filePath, "nonExistingFilePath");
+        assert.strictEqual(filePath, 'nonExistingFilePath');
         return false;
       };
       assert.deepStrictEqual(
         performTail(userArguments, reader, isFilePresent),
         {
-          content: `tail: nonExistingFilePath: No such file or directory`,
-          outputStreamName: "errorStream"
+          content: 'tail: nonExistingFilePath: No such file or directory',
+          outputStreamName: 'errorStream'
         }
       );
     });
