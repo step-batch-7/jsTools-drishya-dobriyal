a7d21d539c9ff08e54f4af7d26581134ced99ee0
Drishya Dobriyal <dobriyaldrishya@gmail.com>
2019-12-28 17:52:35 +0530

    format the code

--- tail.js
+++ tail.js
@@ -1,19 +1,22 @@
-const { performTail } = require("./src/tailLib.js");
+const fs = require('fs');
+const { performTail } = require('./src/tailLib.js');
 
 const getOutputStream = function(outputStreamName) {
-  if (outputStreamName === "errorStream") return console.error;
+  if (outputStreamName === 'errorStream') {
+    return console.error;
+  }
   return console.log;
 };
 
 const main = function() {
-  const userArguments = process.argv.slice(1);
-  const fs = require("fs");
+  const separatedRequiredArgs = 1;
+  const userArguments = process.argv.slice(separatedRequiredArgs);
   const { outputStreamName, content } = performTail(
     userArguments,
     fs.readFileSync,
     fs.existsSync
   );
-  outputStream = getOutputStream(outputStreamName);
+  const outputStream = getOutputStream(outputStreamName);
   outputStream(content);
 };
 
