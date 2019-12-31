e97a62f9a134fefe8d1a5d753b34b35342bfb570
Drishya Dobriyal <dobriyaldrishya@gmail.com>
2019-12-31 11:56:43 +0530

    modified testTailLib

--- test/testTailLib.js
+++ test/testTailLib.js
@@ -31,6 +31,7 @@ describe('performTail', function() {
     performTail(userArguments, fakeReader, onCompletion);
     assert.equal(fakeReader.firstCall.args[0], 'nonExistingFilePath');
     assert.equal(fakeReader.firstCall.args[1], 'utf8');
+    sinon.restore();
   });
   it('should give illegal count error if -n does not have num after it', done => {
     const userArguments = ['-n', '-$', 'filePath'];
@@ -44,7 +45,10 @@ describe('performTail', function() {
   });
   it('should give the asked num of lines from end of a file that exist ', done => {
     const userArguments = ['-n', '-4', 'filePath'];
-    const reader = sinon.fake();
+    const reader = sinon.fake.yieldsAsync(
+      null,
+      '1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n'
+    );
     const onCompletion = (error, content) => {
       assert.strictEqual(error, '');
       assert.strictEqual(content, '7\n8\n9\n10');
@@ -53,12 +57,11 @@ describe('performTail', function() {
     performTail(userArguments, reader, onCompletion);
     assert.equal(reader.firstCall.args[0], 'filePath');
     assert.equal(reader.firstCall.args[1], 'utf8');
-    reader.firstCall.args[2](null, '1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n');
     sinon.restore();
   });
   it('give error if file does not exist with num of lines stated in cmd line args', done => {
     const userArguments = ['-n', '-5', 'nonExistingFilePath'];
-    const reader = sinon.fake();
+    const reader = sinon.fake.yieldsAsync('error', undefined);
     const onCompletion = (error, content) => {
       assert.strictEqual(
         error,
@@ -70,7 +73,6 @@ describe('performTail', function() {
     performTail(userArguments, reader, onCompletion);
     assert.equal(reader.firstCall.args[0], 'nonExistingFilePath');
     assert.equal(reader.firstCall.args[1], 'utf8');
-    reader.firstCall.args[2]('error', undefined);
     sinon.restore();
   });
 });
